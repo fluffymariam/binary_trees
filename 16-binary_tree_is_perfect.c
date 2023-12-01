3479b2a5b80383cb823d7101fef913f514a10ac2
fluffymariam <adefowobiladejobi@gmail.com>
2023-11-30 21:05:22 -0800

    p

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -38,3 +38,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (nodes == perfect_nodes ? 1 : 0);
 }
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
+ *
+ * Return: Size of the binary tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
